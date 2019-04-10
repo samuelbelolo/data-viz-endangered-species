eb3672b1d7b4e4d2c72102c901c6c33e1eb8e78c
samuelbelolo <samuel.belolo@hetic.net>
2019-04-11 01:00:43 +0200

    :sparkles: Added API request for general infos

--- controllers/species.php
+++ controllers/species.php
@@ -8,26 +8,9 @@
      * API
      */
 
-    $redlistUrl = 'https://apiv3.iucnredlist.org/api/v3/country/list?token=9bb4facb6d23f48efbf424bb05c0c1ef1cf6f468393bc745d42179ac4aca5fee';
+    $generalInfosURL = 'https://apiv3.iucnredlist.org/api/v3/species/'.$speciesName.'?token='.token;
 
-    // echo $redlistUrl;
-    $result = ApiRequest($redlistUrl, 604800);
-    $array = array(
-        'country' => array(),
-        'count' => array()
-    );
-    foreach ($result->results as $key => $value) {
-        $url = 'https://apiv3.iucnredlist.org/api/v3/country/getspecies/'.$value->isocode.'?token=9bb4facb6d23f48efbf424bb05c0c1ef1cf6f468393bc745d42179ac4aca5fee';
-        $resultCount = ApiRequest($url, 604800);
-        $array['country'][]=$value->country;
-        $array['count'][]= $resultCount->count;
-
-
-    }
-    for ($i=0; $i <count($array['country']) ; $i++) { 
-        echo $array['country'][$i]. " : ".$array['count'][$i].'</br>';
-    }
-
-    // echo '<pre>';
-    // print_r($result->results);
-    // echo '</pre>';
+    $speciesInfosArray = ApiRequest($generalInfosURL, 604800);
+    echo '<pre>';
+    print_r($speciesInfosArray);
+    echo '</pre>';
