541dbf0a0cb12d9fd7562bc6587579771cb599e7
samuelbelolo <samuel.belolo@hetic.net>
2019-04-11 01:15:41 +0200

    :sparkles: Added API request for habitats infos

--- controllers/species.php
+++ controllers/species.php
@@ -9,11 +9,13 @@
      */
 
     // General Infos
+
     $generalInfosURL = 'https://apiv3.iucnredlist.org/api/v3/species/'.$speciesName.'?token='.token;
 
     $speciesInfosArray = ApiRequest($generalInfosURL, 604800);
 
     // Narrative
+
     $narrativeURL = 'https://apiv3.iucnredlist.org/api/v3/species/narrative/'.$speciesName.'?token='.token;
     $narrativeArray = ApiRequest($narrativeURL, 604800);
 
@@ -21,9 +23,14 @@
 
     $threatsURL= 'https://apiv3.iucnredlist.org/api/v3/threats/species/name/'.$speciesName.'?token='.token;
     $threatsArray = ApiRequest($threatsURL, 604800);
+
+    // Habitats
+
+    $habitatsURL = 'https://apiv3.iucnredlist.org/api/v3/habitats/species/name/'.$speciesName.'?token='.token;
+    $habitatArray = ApiRequest($habitatsURL, 604800);
     
     
     // $result = array_merge($speciesInfosArray->result,$test->result);
     echo '<pre>';
-    print_r($threatsArray);
+    print_r($habitatArray);
     echo '</pre>';
