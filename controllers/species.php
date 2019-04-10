ceba6f1107b1241ef77301d086bee24ee5772a27
samuelbelolo <samuel.belolo@hetic.net>
2019-04-11 01:11:47 +0200

    :sparkles: Added API request for narrative infos

--- controllers/species.php
+++ controllers/species.php
@@ -8,9 +8,19 @@
      * API
      */
 
+    // General Infos
     $generalInfosURL = 'https://apiv3.iucnredlist.org/api/v3/species/'.$speciesName.'?token='.token;
 
     $speciesInfosArray = ApiRequest($generalInfosURL, 604800);
+
+    // Narrative
+    $narrativeURL = 'https://apiv3.iucnredlist.org/api/v3/species/narrative/'.$speciesName.'?token='.token;
+    $narrativeArray = ApiRequest($narrativeURL, 604800);
+
+
+    
+    
+    // $result = array_merge($speciesInfosArray->result,$test->result);
     echo '<pre>';
     print_r($speciesInfosArray);
     echo '</pre>';
