43fac0eeb53c7ae85f781be4635e278af2ff8ca2
samuelbelolo <samuel.belolo@hetic.net>
2019-04-12 00:34:28 +0200

    :sparkles: Added Submit of the form and checked of the input

---
+++ src/js/functions/form_category.js
@@ -0,0 +1,25 @@
+console.log('ok');
+
+/**
+*   Variables 
+*/
+const categoryForm = document.querySelector('.categoryForm')
+const categoryDivElement = [...categoryForm.querySelectorAll('.category')]
+const categoryInput = categoryForm.querySelectorAll('.category input')
+
+
+/**
+*   Submit form 
+*/
+if (categoryForm) {
+    // For each category div
+    for (const key in categoryDivElement) {
+        // On click
+        categoryDivElement[key].addEventListener('click', () => {
+            // Add checked to input type radio
+            categoryInput[key].checked = true;
+            // Submit the form
+            categoryForm.submit()
+        })
+    }
+}
