6369826135e6b424d79e16eca381adeed1ca742b
samuelbelolo <samuel.belolo@hetic.net>
2019-04-11 02:32:47 +0200

    :sparkles: Added Array with country with their isocode and country with their number of species

--- controllers/home.php
+++ controllers/home.php
@@ -2,12 +2,36 @@
 
 $title = 'My Website';
 
+/**
+* API 
+*/
 $countryNameURL = 'https://apiv3.iucnredlist.org/api/v3/country/list?token='.token;
 $countryArray = ApiRequest($countryNameURL, 604800);
 
+$countryInfos = array(
+    'names' => array(),
+    'count' => array()
+);
+foreach ($countryArray->results as $key => $value) {
+    // Add Isocode and Country Name to array
+    $countryInfos['names'][$value->country] = $value->isocode;
+
+    // Get count of species by country
+    $speciesByCountryURL = 'https://apiv3.iucnredlist.org/api/v3/country/getspecies/'.$value->isocode.'?token='.token;
+    $speciesByCountryResult = ApiRequest($speciesByCountryURL, 604800);
+
+    // Add Count to isocode
+    $countryInfos['count'][$value->isocode] = $speciesByCountryResult->count;
+}
+
 echo '<pre>';
-print_r($countryArray);
+print_r($countryInfos);
 echo '</pre>';
+
+
+// echo '<pre>';
+// print_r($countryArray);
+// echo '</pre>';
 
 
 
